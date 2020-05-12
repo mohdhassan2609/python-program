84abd1ee0af1f1f9254a8ffdd6d37fa22a557b47
mohamed hassan <56191446+mohdhassan2609@users.noreply.github.com>
2020-05-12 13:15:46 +0530

    swapcase

---
+++ swapcase.py
@@ -0,0 +1,8 @@
+    #you are a given a string and your task is to swapcase.In otherwords,convert all lowercase letters to uppercase and viceversa....INPUT FORMAT:A single line containing a string S...OUTPUT FORMAT:print the modified string S...
+
+
+s=input()
+result=swap_case(s)
+print(result)
+def swap_case(s)
+ return s.swapcase()
